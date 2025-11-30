a8da6cba411cfd3a846e6a794ba603343719e849
lucas.qin <464815411@qq.com>
2025-11-30 10:51:59 +0800

    用户上传视频组件路由关闭

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   post "download_xiaohongshu" => "welcome#download_xiaohongshu"
 
   resources :videos, only: [ :index, :show, :destroy ]
-  get "video_player", to: "video_player#index"
   post "word_lookup", to: "word_lookup#create"
 
   mount MissionControl::Jobs::Engine, at: "/jobs"
