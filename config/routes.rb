89bc355c75003a0268523d8991f534447420d5b1
lucas.qin <464815411@qq.com>
2025-12-04 20:01:01 +0800

    support pwa

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
   post "word_lookup", to: "word_lookup#create"
 
   mount MissionControl::Jobs::Engine, at: "/jobs"
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 end
