c92bc6be3e7ac06b6a6630e65d42a2e645656d74
lucas.qin <464815411@qq.com>
2025-12-16 21:17:00 +0800

    Reapply "OSS上传放到异步操作，不要影响性能"
    This reverts commit 8a94269bb93224ef2e89163872116c2b7c3753c3.

---
+++ app/jobs/cos_upload_job.rb
@@ -0,0 +1,19 @@
+class CosUploadJob < ApplicationJob
+  queue_as :default
+
+  def perform(video_id)
+    video = Video.find_by(id: video_id)
+    return if video.nil? || video.ori_video_url.blank? || !Rails.env.production?
+
+    response = Typhoeus.post(
+      "http://new_web-cos:8080/api/v1/upload",
+      headers: { "Content-Type" => "application/json" },
+      body: { file_url: video.ori_video_url }.to_json
+    )
+    if response.success?
+      video.video_file.purge_later
+    else
+      Rails.logger.error "OSS 上传失败: #{response.body}, #{video.ori_video_url}"
+    end
+  end
+end
