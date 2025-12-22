4dddf13c689b522ef019540211aee5f44abb7781
秦思成 <464815411@qq.com>
2025-12-22 20:13:12 +0800

    修复本地下载附件错误问题

--- app/jobs/local_upload_job.rb
+++ app/jobs/local_upload_job.rb
@@ -2,9 +2,9 @@ class LocalUploadJob < ApplicationJob
   queue_as :upload
 
   def perform(id)
-    record = Video.find(id)
+    record = Video.find_by(id: id)
     file = Down.download(record.ori_video_url)
-    video_file.attach(io: file, filename: record.title, content_type: Util.determine_content_type(file))
+    record.video_file.attach(io: file, filename: record.title, content_type: Util.determine_content_type(file))
   rescue StandardError => e
     Rails.logger.error "本地上传失败: #{e.message}, #{record.id}"
   end
